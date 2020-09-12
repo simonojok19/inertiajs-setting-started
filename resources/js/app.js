50483fbc4df562396e29b9f0994214409ee5a4d1
simonojok19 <simonojok19@gmail.com>
2020-09-12 12:42:45 +0300

    added the vue and the starting point

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import Inertia from '@inertiajs/inertia-vue';
+import Vue from 'vue'
+
+Vue.use(Inertia);
+let app = document.getElementById('app');
+new Vue({
+    render: (h) => h(Inertia, {
+        props: {
+            initialPage: JSON.parse(app.dataset.page),
+            resolveComponent: name => import(`@/Pages/${name}`).then(module => module.default)
+        },
+    }),
+}).$mount(app);
