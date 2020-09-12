6c08dcce5e77aa66e98d5ade7ccc586f37d555a0
simonojok19 <simonojok19@gmail.com>
2020-09-12 14:31:23 +0300

    added users index vue

---
+++ resources/js/Users/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <Layout>
+        <div>
+            This is the About Page: {{ foo }}
+        </div>
+    </Layout>
+</template>
+<script>
+import Layout from '../Shared/Layout.vue'
+export default {
+    props: ['foo'],
+    components: {
+        Layout,
+    }
+}
+</script>
