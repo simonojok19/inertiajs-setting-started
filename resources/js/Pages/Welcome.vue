b67ebdc154113dc0e72e2ffc1647eb7ef72661b5
simonojok19 <simonojok19@gmail.com>
2020-09-12 13:13:43 +0300

    wiring up the component

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        This is the welcome Page
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return Inertia\Inertia::render(
-        'Event/Show',
+        'Welcome', [
+            'foo' => 'bar'
+        ]
     );
 });
