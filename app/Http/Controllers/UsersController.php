8e73ef726aac16c96bd10b79d284d6087ce593ba
simonojok19 <simonojok19@gmail.com>
2020-09-12 14:30:16 +0300

    added users controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $users = User::all();
+        return Inertia::render(
+            'Users/Index',
+            ['users' => $users]
+        );
     }
 
     /**
